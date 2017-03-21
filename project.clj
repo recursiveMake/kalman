f1e70feb86da67d03b9ac09304dcbed94166518e
Adonis Bovell <Adonis.Bovell@gtri.gatech.edu>
2017-03-21 12:54:07 -0400

    Add plotting along with sample data

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [incanter/incanter-core "1.9.1"]]
+                 [incanter/incanter-core "1.9.1"]
+                 [incanter/incanter-charts "1.9.1"]
+                 [incanter/incanter-io "1.9.1"]]
   :main ^:skip-aot kalman.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/kalman/core.clj
+++ src/kalman/core.clj
@@ -1,7 +1,51 @@
 (ns kalman.core
   (:require [kalman.filter :refer [kalman]]
-            [kalman.system :refer [set-system-parameter, create-system]])
+            [kalman.system :refer [set-system-parameter, create-system]]
+            [kalman.io :refer [read-file, lazy-read-file, cast-line, annotate-row]]
+            [clojure.core.matrix :as m]
+            [incanter.core :refer [view]]
+            [incanter.charts :refer [scatter-plot]]
+            [incanter.io :refer [read-dataset]])
   (:gen-class))
+
+(def sample-data (map annotate-row
+                      (map #(cast-line % #",")
+                           (rest (read-file "resources/data.txt")))))
+
+(def observables (map #(m/matrix [(:x-observable %) (:y-observable %)]) sample-data))
+
+(def initial-state (first observables))
+
+(def system
+  (set-system-parameter
+   (create-system 4 2)
+   {:state (m/matrix [(first initial-state) (second initial-state) 0 0])
+    :state-variance (m/matrix [
+                               [ 1   0   0   0 ]
+                               [ 0   1   0   0 ]
+                               [ 0   0   1   0 ]
+                               [ 0   0   0   1 ]])
+    :state-predict (m/matrix [
+                              [1 0 1 0]
+                              [0 1 0 1]
+                              [0 0 1 0]
+                              [0 0 0 1]])
+    ; :state-noise (m/mmul 0.1 (m/identity-matrix 4))
+    :state-noise (m/matrix [
+                            [0.1  0  0.1  0 ]
+                            [ 0  0.1  0  0.1]
+                            [0.1  0  0.1  0 ]
+                            [ 0  0.1  0  0.1]])
+    :sensor-variance (m/matrix [
+                                [1 0 1 0]
+                                [0 1 0 1]])
+    :sensor-noise (m/matrix [
+                             [0.1  0 ]
+                             [ 0  0.1]])}))
+
+(def data (read-dataset "resources/data.txt" :header true))
+
+(view (scatter-plot :x-velocity :y-velocity :data data))
 
 (defn -main
   ""
